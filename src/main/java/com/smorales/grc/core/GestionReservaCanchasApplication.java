4be2e7156ece6f1ec2a9844b6486e70ae17eee5c
Sergio Morales <yeyox_114@hotmail.com>
2018-04-04 14:00:48 -0500

    Inicio del proyecto, integracion de Spring Boot con Angular

---
+++ src/main/java/com/smorales/grc/core/GestionReservaCanchasApplication.java
@@ -0,0 +1,12 @@
+package com.smorales.grc.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionReservaCanchasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionReservaCanchasApplication.class, args);
+	}
+}
